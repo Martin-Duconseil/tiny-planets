6ebbe719985c96b10ded16eaa3ab1c1122329fba
Martin <martin.duconseil@gmail.com>
2026-03-06 17:52:03 +0100

    Really basic menu

---
+++ scripts/button.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var button_type = ""
+
+@export var texture_normal: Resource
+@export var texture_hover: Resource
+
+var button_hovered = false
+var button_clicked = false
+
+func _ready() -> void:
+	pass
+
+func _process(_delta):
+	if button_hovered:
+		$Texture.texture = texture_hover
+		if Input.is_action_pressed("left_click"):
+			button_clicked = true
+	else:
+		$Texture.texture = texture_normal
+	if button_clicked:
+		Global.menu_switch = button_type
+
+func _on_area_2d_mouse_entered():
+	button_hovered = true
+
+func _on_area_2d_mouse_exited():
+	button_hovered = false
